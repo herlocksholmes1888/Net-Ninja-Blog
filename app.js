2e3507c3a6182da555a679f1eae0e22af2e8142a
Luisa Lopes <luisafrugoli11@gmail.com>
2024-08-19 16:22:10 -0300

    :sparkles: show posts on index

--- app.js
+++ app.js
@@ -13,18 +13,24 @@ app.listen(port, () => {
 
 // GET Requests
 app.get("/", (req, res) => {
-    res.render("index");
+    const blogs = [
+        { title: "Top 10 Reasons Why Sonic is Sorry", snippet: "Lorem ipsum dolor sit amet consectetur" },
+        { title: "Eggman's Callout Post", snippet: "Lorem ipsum dolor sit amet consectetur" },
+        { title: "Shadow the Hedgehog Seen at Hot Topic", snippet: "Lorem ipsum dolor sit amet consectetur" },
+    ];
+
+    res.render("index", { home: "Home", blogs });
 });
 
 app.get("/about", (req, res) => {
-    res.render("about");
+    res.render("about", { about: "About" });
 });
 
 app.get("/blogs/create", (req, res) => {
-    res.render("create");
+    res.render("create", { create: "Create" });
 });
 
 // 404
 app.use((req, res) => {
-    res.status(404).render("404");
+    res.status(404).render("404"), { error: "404" };
 });

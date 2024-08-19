8b285bc61c56b508b023d08de39a2a7106f5076a
Luisa Lopes <luisafrugoli11@gmail.com>
2024-08-19 16:55:50 -0300

    :lipstick: add css to head partial

--- app.js
+++ app.js
@@ -19,18 +19,18 @@ app.get("/", (req, res) => {
         { title: "Shadow the Hedgehog Seen at Hot Topic", snippet: "Lorem ipsum dolor sit amet consectetur" }
     ];
 
-    res.render("index", { blogs });
+    res.render("index", { title: "Home", blogs });
 });
 
 app.get("/about", (req, res) => {
-    res.render("about");
+    res.render("about", { title: "About" });
 });
 
 app.get("/blogs/create", (req, res) => {
-    res.render("create");
+    res.render("create", { title: "New Blog" });
 });
 
 // 404
 app.use((req, res) => {
-    res.status(404).render("404");
+    res.status(404).render("404", { title: "404" });
 });

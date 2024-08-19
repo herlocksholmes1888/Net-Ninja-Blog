dc2e46d3b2c1aa1e638c1b9908c24972a98427d8
Luisa Lopes <luisafrugoli11@gmail.com>
2024-08-19 16:01:17 -0300

    :sparkles: add html/ejs to index

--- app.js
+++ app.js
@@ -3,12 +3,15 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+// View Engine 
 app.set("view engine", "ejs");
 
+// Port
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
 });
 
+// GET Requests
 app.get("/", (req, res) => {
     res.render("index");
 });
@@ -21,6 +24,7 @@ app.get("/blogs/create", (req, res) => {
     res.render("create");
 });
 
+// 404
 app.use((req, res) => {
     res.status(404).render("404");
 });
